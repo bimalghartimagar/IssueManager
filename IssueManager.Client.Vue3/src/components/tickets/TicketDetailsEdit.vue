1cbbbd50f1487bad72270f870cf3a8bff74d09ff
Bimal Gharti Maagar <bimal@endpoint.com>
2021-09-03 01:34:40 +0545

    Update ticket edit form
    * Made editable fields more prominent
    * Added icon and spinner for button

--- IssueManager.Client.Vue3/src/components/tickets/TicketDetailsEdit.vue
+++ IssueManager.Client.Vue3/src/components/tickets/TicketDetailsEdit.vue
@@ -4,16 +4,41 @@
             Details of ticket #{{ ticket.id }}
             <button
                 @click="saveTicket"
-                class="float-right rounded px-2 pt-1 bg-white shadow-xl hover:shadow-none hover:bg-indigo-500 hover:text-white"
-            >Save</button>
+                class="flex flex-row items-center float-right rounded px-2 pt-1 bg-white shadow-xl hover:shadow-none hover:bg-indigo-500 hover:text-white"
+                :class="isUpdating ? 'disabled:opacity-50' : ''"
+            >
+                <svg
+                    v-show="isUpdating"
+                    class="mr-1 h-5 w-5 hover:text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                >
+                    <circle
+                        class="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        stroke-width="4"
+                    />
+                    <path
+                        class="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    />
+                </svg>
+                <SaveIcon v-show="!isUpdating" class="mr-1 w-5 h-5" />
+                <span>Save</span>
+            </button>
             <span class="float-right text-white mr-2">{{ updateMsg }}</span>
         </div>
         <div class="grid grid-cols-3 gap-4 shadow p-3">
             <div>Created At: {{ getReadableDateTime(ticket.createdAt) }}</div>
             <div>Updated At: {{ getReadableDateTime(ticket.updatedAt) }}</div>
-            <div>
-                Status:
-                <select v-model="ticket.status" class="p-1 rounded">
+            <div class="flex flex-row">
+                <div class="mr-1">Status:</div>
+                <select v-model="ticket.status" class="p-1 rounded border border-black">
                     <option
                         v-for="data in StatusData"
                         :value="data.value"
@@ -23,11 +48,16 @@
             </div>
         </div>
         <div class="m-4 p-2 rounded shadow text-left border border-gray-400">
+            <div class="text-xl font-bold">Title:</div>
             <div class="p-4 font-semibold text-xl bg-gray-400">
-                <input v-model="ticket.title" class="p-2 w-full border rounded" />
+                <input v-model="ticket.title" class="p-2 w-full rounded border border-black" />
             </div>
             <div class="p-4 m-4">
-                <textarea v-model="ticket.description" class="w-full h-60 p-1 rounded"></textarea>
+                <div class="text-xl font-bold">Description:</div>
+                <textarea
+                    v-model="ticket.description"
+                    class="w-full h-60 p-1 rounded border border-black"
+                ></textarea>
             </div>
         </div>
         <div class="text-left m-4 p-2">
@@ -48,7 +78,7 @@ import { onMounted, ref, watch } from "@vue/runtime-core"
 import TicketService from "../../service/TicketService.js"
 import service from "../../service/TicketService.js"
 import util from "../../utils/index.js"
-
+import { SaveIcon } from "@heroicons/vue/outline"
 const StatusData = [
     { value: "open", name: "Open" },
     { value: "assigned", name: "Assigned" },
@@ -68,15 +98,21 @@ const props = defineProps({
 
 let ticket = ref({})
 let updateMsg = ref("")
+let isUpdating = ref(false)
 
 watch(() => props.id, async (val, prevVal) => {
     if (val !== prevVal) {
-        ticket.value = await service.getTicket(props.id).then(response => response.json())
+        await fetchTicketData()
     }
 })
 
+async function fetchTicketData() {
+    isUpdating.value = true
+    ticket.value = await service.getTicket(props.id).then(response => response.json()).finally(() => isUpdating.value = false)
+}
+
 onMounted(async () => {
-    ticket.value = await service.getTicket(props.id).then(response => response.json())
+    await fetchTicketData()
 })
 
 function getReadableDateTime(dateTimeString) {
@@ -87,7 +123,8 @@ function getReadableDateTime(dateTimeString) {
 }
 
 async function saveTicket() {
-    let response = await TicketService.updateTicket(ticket.value, props.id)
+    isUpdating.value = true
+    let response = await TicketService.updateTicket({ ...ticket.value, updatedAt: (new Date).toISOString() }, props.id)
         .then(res => {
             if (res.status === 200) {
                 return res.json()
@@ -95,8 +132,10 @@ async function saveTicket() {
                 throw new Error("Error while updating ticket details")
             }
         })
-        .then(data => updateMsg.value = "Updated ticket details")
+        .then(async data => await fetchTicketData())
+        .then(() => updateMsg.value = "Updated ticket details")
         .catch(err => updateMsg.value = err)
+        .finally(() => isUpdating.value = false)
 }
 
 </script>
