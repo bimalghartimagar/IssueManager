c3e8bc33b5d38a6ec425e881fe78b2b5b4777e82
Bimal Gharti Maagar <bimal@endpoint.com>
2021-10-31 23:17:51 +0545

    Added pages and components to blazor server
    * Home page works with exact design

--- IssueManager.Client.Blazor.Server/Startup.cs
+++ IssueManager.Client.Blazor.Server/Startup.cs
@@ -1,15 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using IssueManager.Client.Blazor.Server.Data;
+using IssueManager.ThirdParty.Api.Interface;
 
 namespace IssueManager.Client.Blazor.Server
 {
@@ -29,6 +24,7 @@ namespace IssueManager.Client.Blazor.Server
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<ITicketService, TicketService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ IssueManager.Client.Blazor.Server/Util/Utility.cs
@@ -0,0 +1,27 @@
+namespace IssueManager.Client.Blazor.Server.Util
+{
+    public static class Utility
+    {
+        public static string GetStatusColor(string status)
+        {
+            if (status == null)
+            {
+                return "";
+            }
+            if (status.Equals("open"))
+            {
+                return "bg-yellow-500 font-semibold";
+            }
+            else if (status.Equals("assigned"))
+            {
+                return "bg-green-500 font-semibold";
+            }
+            else if (status.Equals("closed"))
+            {
+                return "bg-red-500 font-semibold";
+            }
+            return "";
+
+        }
+    }
+}
